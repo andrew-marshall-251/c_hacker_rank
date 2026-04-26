20617a88f76f5805fa1e07add63b44a5863c49dc
Andrew Martinez <andrewmartinez8000@gmail.com>
2026-04-26 12:44:42 -0700

    learned how to use scanf

---
+++ playing_with_char.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    char c;
+    char s[100];
+    char sen[100];
+    
+    scanf("%c", &c);
+    scanf("%s", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", sen);
+    
+    printf("%c\n", c);
+    printf("%s\n", s);
+    printf("%s\n", sen);
+    return 0;
+}
