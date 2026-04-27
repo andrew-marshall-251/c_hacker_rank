4ad60d1666b7af6020e4dda0530984da8c5d72d6
Andrew Martinez <andrewmartinez8000@gmail.com>
2026-04-27 08:08:52 -0700

    stack buffer

---
+++ max_of_four.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+Add `int max_of_four(int a, int b, int c, int d)` here.
+*/
+int max_of_four(int a, int b, int c, int d) {
+    int four[4] = {a, b, c, d};
+    int max = a;
+    for (int i = 0; i < 4; i++) {
+        if (max <= four[i]) {
+            max = four[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
